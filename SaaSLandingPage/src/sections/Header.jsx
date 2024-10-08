aad6e0af5173f4efbe283aec1ad07e734a9b9a93
Ritesh-16 <70386267+Ritesh-16@users.noreply.github.com>
2024-10-08 19:01:40 +0530

    deploy

--- SaaSLandingPage/src/sections/Header.jsx
+++ SaaSLandingPage/src/sections/Header.jsx
@@ -34,7 +34,7 @@ const Header =() => {
         <header className={clsx('fixed top-0 left-0 py-10 z-50 w-full transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
             <div className='container flex h-14 items-center max-lg:px-5'>
                 <a className='lg:hidden flex-1 cursor-pointer z-2'>
-                    <img src="/images/xora.svg" alt="logo" width={115} height={55} />
+                    <img src="images/xora.svg" alt="logo" width={115} height={55} />
                 </a>
                 <div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', isOpen ? 'max-lg:opacity-100' : 'max-lg:opacity-0 max-lg:pointer-events-none')}>
                 <div className='max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4'>
@@ -62,16 +62,16 @@ const Header =() => {
                         </ul>
                     </nav>
                     <div className='lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90'>
-                        <img src="/images/bg-outlines.svg" width={960} height={380} alt="outline" className='relative z-2' />
+                        <img src="images/bg-outlines.svg" width={960} height={380} alt="outline" className='relative z-2' />
 
-                        <img src="SaaS-Landing-Page\SaaSLandingPage\public\images\bg-outlines.svg" width={960} height={380} alt="outline" className='absolute inset-0 mix-blend-soft-light opacity-5' />
+                        <img src="images/bg-outlines-fill.png" width={960} height={380} alt="outline" className='absolute inset-0 mix-blend-soft-light opacity-5' />
                     </div>
                      
                 </div>
                 </div>
                 <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center" onClick={() => setIsOpen((prevState) => !prevState)}>
                 <img 
-                src={`/images/${isOpen ? 'close' : 'magic'}.svg` } alt="magic" 
+                src={`images/${isOpen ? 'close' : 'magic'}.svg` } alt="magic" 
                 className='size-1/2 object-contain' /></button>
             </div>
             </header>
